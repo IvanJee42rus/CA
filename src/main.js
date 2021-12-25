eb04f0c657954859ec766e5026e857441169b244
Valerii Shabalin <vshabalin@sinergo.ru>
2021-12-25 14:44:11 +0700

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import Vue from 'vue';
+import vuetify from '@/plugins/vuetify';
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    vuetify,
+}).$mount('#app');
