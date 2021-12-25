d0bd4d2f98d95883b3ce84365505081c1aa36763
Valerii Shabalin <vshabalin@sinergo.ru>
2021-12-25 17:04:05 +0700

    update main form

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
-    <FormStepper />
+    <v-app>
+        <v-container>
+            <v-row>
+                <v-col cols="12">
+                    <FormStepper />
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-app>
 </template>
 
 <script>
 export default {
     name: 'App',
-    components: {},
+    components: {
+        FormStepper: () => (import('./components/form-stepper.vue'))
+    },
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+    #app {
+        padding: 100px 0;
+    }
+</style>
--- src/components/form-stepper.vue
+++ src/components/form-stepper.vue
@@ -1,101 +1,82 @@
 <template>
-    <v-stepper v-model="e1">
-        <v-stepper-header>
-            <v-stepper-step
-                :complete="e1 > 1"
-                step="1"
-            >
-                Name of step 1
-            </v-stepper-step>
+    <v-stepper v-model="currentStep" vertical>
+        <v-stepper-step :complete="currentStep > 1" step="1">
+            Варианты и эксперты
+        </v-stepper-step>
 
-            <v-divider></v-divider>
+        <v-stepper-step :complete="currentStep > 2" step="2">
+            Варианты управления
+        </v-stepper-step>
 
-            <v-stepper-step
-                :complete="e1 > 2"
-                step="2"
-            >
-                Name of step 2
-            </v-stepper-step>
+        <v-stepper-step step="3">
+            Экспертные оценки
+        </v-stepper-step>
 
-            <v-divider></v-divider>
-
-            <v-stepper-step step="3">
-                Name of step 3
-            </v-stepper-step>
-        </v-stepper-header>
-
-        <v-stepper-items>
         <v-stepper-content step="1">
-            <v-card
-            class="mb-12"
-            color="grey lighten-1"
-            height="200px"
-            ></v-card>
-
-            <v-btn
-            color="primary"
-            @click="e1 = 2"
-            >
-            Continue
-            </v-btn>
-
-            <v-btn text>
-            Cancel
-            </v-btn>
+            <v-card elevation="2" outlined>
+                <v-card-title>Количество вариантов и экспертов</v-card-title>
+                <v-card-subtitle>Укажите число вариантов управления и количество экспертов для формирования матриц. Значения должны быть целыми числами больше нуля.</v-card-subtitle>
+                <v-card-text>
+                    <v-row>
+                        <v-col>
+                            <v-text-field v-model="countControl" label="Количество вариантов управления"></v-text-field>
+                        </v-col>
+                        <v-col>
+                            <v-text-field v-model="countExpert" label="Количество экспертов"></v-text-field>
+                        </v-col>
+                    </v-row>
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn color="primary" @click="setCurrentStep(2)">Продолжить</v-btn>
+                </v-card-actions>
+            </v-card>
         </v-stepper-content>
 
         <v-stepper-content step="2">
-            <v-card
-            class="mb-12"
-            color="grey lighten-1"
-            height="200px"
-            ></v-card>
-
-            <v-btn
-            color="primary"
-            @click="e1 = 3"
-            >
-            Continue
-            </v-btn>
-
-            <v-btn text>
-            Cancel
-            </v-btn>
+            <v-card elevation="2" outlined>
+                <v-card-title>Количество вариантов и экспертов</v-card-title>
+                <v-card-subtitle>Укажите число вариантов управления и количество экспертов для формирования матриц. Значения должны быть целыми числами больше нуля.</v-card-subtitle>
+                <v-card-actions>
+                    <v-btn color="primary" @click="setCurrentStep(3)">Продолжить</v-btn>
+                    <v-btn text @click="setCurrentStep(1)">Вернуться</v-btn>
+                </v-card-actions>
+            </v-card>
         </v-stepper-content>
 
         <v-stepper-content step="3">
-            <v-card
-            class="mb-12"
-            color="grey lighten-1"
-            height="200px"
-            ></v-card>
-
-            <v-btn
-            color="primary"
-            @click="e1 = 1"
-            >
-            Continue
-            </v-btn>
-
-            <v-btn text>
-            Cancel
-            </v-btn>
+            <v-card elevation="2" outlined>
+                <v-card-actions>
+                    <v-btn color="primary" @click="calculate()">Вычислить</v-btn>
+                    <v-btn text @click="setCurrentStep(2)">Вернуться</v-btn>
+                </v-card-actions>
+            </v-card>
         </v-stepper-content>
-        </v-stepper-items>
     </v-stepper>
 </template>
 
 <script>
     export default {
         name: 'FormStepper',
-        data () {
-        return {
-                e1: 1,
+        data: () => ({
+            currentStep: 1,
+            countControl: 0,
+            countExpert: 0
+        }),
+        methods: {
+            setCurrentStep(step) {
+                if (step < 1 || step > 3) return;
+                this.currentStep = step;
+            },
+            calculate() {
+                console.log('calc');
             }
-        },
+        }
     }
 </script>
 
 <style lang="scss" scoped>
-
+    .v-card {
+        padding: 20px;
+        margin: 20px 0;
+    }
 </style>
